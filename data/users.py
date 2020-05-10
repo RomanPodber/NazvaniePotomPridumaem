b7e6fb1afef7290268798a72628507eef06b02ee
RomanPodber <roman.pod2016@yandex.ru>
2020-05-10 15:29:04 +0300

    Наконец-то!

--- data/users.py
+++ data/users.py
@@ -1,10 +1,10 @@
 import datetime
 import sqlalchemy
-from flask import Flask
 from data.db_session import SqlAlchemyBase
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = 'z_k.,k._gbwwe'
 
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
@@ -19,5 +19,19 @@ class User(SqlAlchemyBase):
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
 
-if __name__ == '__main__':
-    app.run(port=8000, host='127.0.0.16')
+class News(SqlAlchemyBase):
+    __tablename__ = 'news'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
+
+    news = orm.relation("News", back_populates='user')
