7cbf5b304cb9999db34d2a8032cbacd77625ce92
RomanPodber <roman.pod2016@yandex.ru>
2020-05-10 11:33:25 +0300

    база данных2

--- data/users.py
+++ data/users.py
@@ -1,17 +1,35 @@
 import datetime
 import sqlalchemy
+from sqlalchemy import orm
 from data.db_session import SqlAlchemyBase
+from data import db_session
+from flask import render_template, redirect, Flask
+
+app = Flask(__name__)
 
 
-class User(SqlAlchemyBase):
-    __tablename__ = 'users'
+class News(SqlAlchemyBase):
+    __tablename__ = 'news'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String,
-                              index=True, unique=True, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
+    news = orm.relation("News", back_populates='user')
+
+@app.route("/")
+def index():
+    session = db_session.create_session()
+    news = session.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
+if __name__ == '__main__':
+    app.run(port=8000, host='127.0.0.16')
